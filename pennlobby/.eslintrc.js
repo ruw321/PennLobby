23fca927becb7e64b99785d9dc2298ca1463c06a
Ruichen Zhang <ruichen199801@gmail.com>
2021-11-10 13:38:43 -0500

    Add MUI components to Group.js

--- pennlobby/.eslintrc.js
+++ pennlobby/.eslintrc.js
@@ -4,21 +4,20 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ["plugin:react/recommended", "airbnb"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2020,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react"],
   rules: {
-    "react/jsx-filename-extension": "off"
+    "react/jsx-filename-extension": "off",
+    quotes: "off",
+    "react/jsx-one-expression-per-line": "off",
+    "implicit-arrow-linebreak": "off",
+    "no-confusing-arrow": "off",
   },
 };
--- pennlobby/src/components/Group.js
+++ pennlobby/src/components/Group.js
@@ -1,9 +1,59 @@
-import React, { useState } from "react";
+import * as React from "react";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { Container, Box } from "@mui/material";
 
 function Group() {
-  // const [userName, setUserName] = useState("");
+  const [groupName] = useState("Penn Football Club");
 
-  return <div className="group">This is the group placeholder</div>;
+  const buttons = [
+    <Button key="one">One</Button>,
+    <Button key="two">Two</Button>,
+    <Button key="three">Three</Button>,
+  ];
+
+  return (
+    <Container maxWidth="sm" className="group">
+      <div className="group-text">
+        <h1>{groupName}</h1>
+      </div>
+      {/* <img src="pic_trulli.jpg" alt="Italian Trulli">
+        Image Placeholder
+      </img> */}
+      <Box
+        sx={{
+          display: "flex",
+          "& > *": {
+            m: 1,
+          },
+        }}
+      >
+        <ButtonGroup
+          orientation="vertical"
+          aria-label="vertical outlined button group"
+        >
+          {buttons}
+        </ButtonGroup>
+        <ButtonGroup
+          orientation="vertical"
+          aria-label="vertical contained button group"
+          variant="contained"
+        >
+          {buttons}
+        </ButtonGroup>
+        <ButtonGroup
+          orientation="vertical"
+          aria-label="vertical contained button group"
+          variant="text"
+        >
+          {buttons}
+        </ButtonGroup>
+      </Box>
+      const buttons = [<Button key="one">One</Button>,
+      <Button key="two">Two</Button>,<Button key="three">Three</Button>, ];
+    </Container>
+  );
 }
 
 export default Group;
