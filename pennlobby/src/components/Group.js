da7a4f0c50d90e753a6e335935e2d8a5d9f47828
Ruichen Zhang <ruichen199801@gmail.com>
2021-11-10 10:31:10 -0500

    Add group

---
+++ pennlobby/src/components/Group.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+function Group() {
+  // const [userName, setUserName] = useState("");
+
+  return <div className="group">This is the group placeholder</div>;
+}
+
+export default Group;
